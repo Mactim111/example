b96e5b4d244bcf682633d6fa90d7dc279cc0ad0a
mactim111 <mactim@inbox.ru>
2025-11-27 15:43:49 +0300

    edit ruote3

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('welcome2', ['users']);
 });
+Route::get('/', function () {
+    return view('welcome3', ['users']);
+});
