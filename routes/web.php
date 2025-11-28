11b88a1ac03c07240fcb3674ac984457f90ed965
mactim111 <mactim@inbox.ru>
2025-11-28 14:07:44 +0300

    edit ruote6

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
-    return view('welcome2', ['users']);
+    return view('welcome', ['users']);
 });
 Route::get('/', function () {
     return view('welcome2', ['users']);
 });
 Route::get('/', function () {
     return view('welcome3', ['users']);
+});
+Route::get('/1', function () {
+    return view('welcome4', ['users']);
+});
+
+Route::get('/1', function () {
+    return view('welcome5', ['users']);
 });
